2dd6b5e21cd2d5596ecfc24b952bdb61778f25dc
Timur <sasintimur@gmail.com>
2021-02-03 23:10:20 +0300

    Lesson Seven was added

--- src/main/java/lesson7/MainApp.java
+++ src/main/java/lesson7/MainApp.java
@@ -1,4 +1,20 @@
 package lesson7;
 
 public class MainApp {
+
+    public static void main(String[] args) {
+
+        Cat[] cats = {
+                new Cat("Barsik", 50),
+                new Cat("Leo", 30),
+                new Cat("Simba", 25),
+        };
+
+        Plate plate = new Plate(100);
+
+        for (Cat i : cats) {
+            i.eat(plate);
+            i.info();
+        }
+    }
 }
