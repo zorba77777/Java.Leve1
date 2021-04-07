62dc68e2c2efeeaeb7f10460a3225f591e408182
Timur <sasintimur@gmail.com>
2021-04-07 23:34:10 +0300

    Lesson21 was added

---
+++ src/main/java/lesson21/Race.java
@@ -0,0 +1,16 @@
+package lesson21;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private final ArrayList<Stage> stages;
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
